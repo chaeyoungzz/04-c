27103c929669830f198bde2f5ef36ac46640cca2
chaeyoung <456codud@naver.com>
2018-10-11 15:23:42 +0900

    +/-

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	
+	printf("number : ");
+	scanf("%i",&a);
+	
+	if (a>0)
+	   printf("양수입니다.");	
+	   
+	else if (a<0)
+	   printf("음수입니다.");
+	   
+	else 
+	   printf("0");
+	   
+	return 0;
+}
