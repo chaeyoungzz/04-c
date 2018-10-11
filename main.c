b3d64b59c1c39ff7f3c835a617ad3b98d2b15d91
chaeyoung <456codud@naver.com>
2018-10-11 15:36:55 +0900

    ㅣㅣ

--- main.c
+++ main.c
@@ -5,18 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	int a;
-	
-	printf("number : ");
+	int f; 
+	printf("정수 하나를 입력하시오 : ");
 	scanf("%i",&a);
 	
-	if (a>0)
-	   printf("양수입니다.");	
+	if (a<0)
+	   f=-a;
+	else
+	   f=a;
+	
+	printf("절댓값은 %i 입니다.",f);
 	   
-	else if (a<0)
-	   printf("음수입니다.");
-	   
-	else 
-	   printf("0");
 	   
 	return 0;
 }
